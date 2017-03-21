4c9ab7d112cf9b45ce1fa8cff020ffe836a697de
rcmalli <refikcanmalli@gmail.com>
2017-03-21 11:19:22 +0100

    UPDATE new version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='keras_squeezenet',
-      version='0.2',
+      version='0.3',
       description='Squeezenet implementation with Keras framework',
       url='https://github.com/rcmalli/keras-squeezenet',
       author='Refik Can MALLI',
