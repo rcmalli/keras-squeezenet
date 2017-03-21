654ac56861ae2cb4b805aa201c900b5984a0e1b9
rcmalli <refikcanmalli@gmail.com>
2017-03-21 11:45:12 +0100

    ADD travis
    test integration setup

---
+++ test.py
@@ -0,0 +1,14 @@
+import numpy as np
+from keras_squeezenet import SqueezeNet
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.preprocessing import image
+
+model = SqueezeNet()
+
+img = image.load_img('../images/cat.jpeg', target_size=(227, 227))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+preds = model.predict(x)
+print('Predicted:', decode_predictions(preds))
