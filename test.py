3e23b7f1ddc221fcbb5530234cfa0faebe5aa0c5
Refik Can Malli <refikcanmalli@gmail.com>
2017-03-21 12:20:55 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,11 +4,3 @@ from keras.applications.imagenet_utils import preprocess_input, decode_predictio
 from keras.preprocessing import image
 
 model = SqueezeNet()
-
-img = image.load_img('../images/cat.jpeg', target_size=(227, 227))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
-
-preds = model.predict(x)
-print('Predicted:', decode_predictions(preds))
